0e58a1f318df84ccfc6d57a554c4347a8c65b7af
Alejandra212S <alejandramin1340@gmail.com>
2026-06-22 17:19:14 -0600

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,127 @@
+import { auth } from "./firebase.js";
+
+import {
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+
+// =========================
+// LOGIN
+// =========================
+
+const formLogin = document.getElementById("formLogin");
+
+if (formLogin) {
+
+    formLogin.addEventListener("submit", async (e) => {
+
+        e.preventDefault();
+
+        const correo =
+            document.getElementById("loginCorreo").value.trim();
+
+        const password =
+            document.getElementById("loginPassword").value.trim();
+
+        try {
+
+            const credencial =
+                await signInWithEmailAndPassword(
+                    auth,
+                    correo,
+                    password
+                );
+
+            localStorage.setItem(
+                "sesionActiva",
+                JSON.stringify({
+                    uid: credencial.user.uid,
+                    correo: credencial.user.email
+                })
+            );
+
+            alert("Inicio de sesión exitoso");
+
+            window.location.href = "inicio.html";
+
+        } catch (error) {
+
+            console.error("Error Login:", error);
+
+            alert("Correo o contraseña incorrectos");
+
+        }
+
+    });
+
+}
+
+// =========================
+// PROTEGER PAGINA
+// =========================
+
+window.protegerPagina = function () {
+
+    onAuthStateChanged(auth, (user) => {
+
+        if (!user) {
+
+            window.location.href = "login.html";
+
+        } else {
+
+            const nombreUsuario =
+                document.getElementById("nombreUsuario");
+
+            if (nombreUsuario) {
+
+                nombreUsuario.textContent =
+                    user.email;
+
+            }
+
+        }
+
+    });
+
+};
+
+// =========================
+// REDIRECCION SI YA INICIO SESION
+// =========================
+
+onAuthStateChanged(auth, (user) => {
+
+    if (
+        user &&
+        window.location.pathname.includes("login.html")
+    ) {
+
+        window.location.href = "inicio.html";
+
+    }
+
+});
+
+// =========================
+// CERRAR SESION
+// =========================
+
+window.cerrarSesion = async function () {
+
+    try {
+
+        await signOut(auth);
+
+        localStorage.removeItem("sesionActiva");
+
+        window.location.href = "login.html";
+
+    } catch (error) {
+
+        console.error("Error al cerrar sesión:", error);
+
+    }
+
+};
